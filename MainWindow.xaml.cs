8d0a23f25636834d5b72184c3b00fb2f0d5f5d3a
Brodyaga1238 <nurlanhasanov1238@gmail.com>
2023-12-29 04:03:29 +0300

    Update on mvvm

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,33 +17,74 @@ using GTranslate.Translators;
 
 namespace TranslatorForComputer
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    public class TranslateViewModel : INotifyPropertyChanged
+    {
+        private string? _words;
+        public string Words
+        {
+            get { return _words; }
+            set
+            {
+                if (_words != value)
+                {
+                    _words = value;
+                    OnPropertyChanged(nameof(Words));
+                    Translate();
+                }
+            }
+        }
+
+        private string? _translation;
+        public string Translation
+        {
+            get { return _translation; }
+            set
+            {
+                if (_translation != value)
+                {
+                    _translation = value;
+                    OnPropertyChanged(nameof(Translation));
+                }
+            }
+        }
+
+        public async Task Translate()
+        {
+            var translator = new YandexTranslator();
+            if (string.IsNullOrEmpty(Words))
+            {
+                Translation = null;
+                return;
+            }
+            var result = await translator.TranslateAsync(Words, "En");
+            Translation = result.Translation;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+
+
     public partial class MainWindow : Window
     {
-        string? words;
+        private readonly TranslateViewModel _viewModel;
+
         public MainWindow()
         {
             InitializeComponent();
+            _viewModel = new TranslateViewModel();
+            DataContext = _viewModel;
+        }
+        private void TranslateBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            _viewModel.Translate();
+        }
 
-        }
-        private async Task Translate()
-        {
-            var translator = new YandexTranslator();
-            words =  TranslateBox.Text;
-            if (words=="")
-            {
-                ResultBox.Text = "";
-                return;
-            }
-            var result = await translator.TranslateAsync( words, "En");
-            ResultBox.Text = result.Translation;
-        }
-     
-        private async void TranslateBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            await Translate();
-        }
     }
+
 }
